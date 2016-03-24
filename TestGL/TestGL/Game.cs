2fbcf6cb90c466d0badc3b80d4bc8215677b0b05
Chris N <swordsman9613@gmail.com>
2016-03-24 12:52:27 -1000

    Initial Commit
    Whyyyyyyyy

---
+++ TestGL/TestGL/Game.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK;
+//using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace TestGL
+{
+    class Game
+    {
+        // String denoting what the current status of the game is, i.e. paused, at menu, loading, running...
+        public string state;
+
+        // Ints for height and width of the screen. (Should be set in room designer.)
+        public int height, width;
+
+        public SpriteRenderer Renderer = new SpriteRenderer();
+
+        // Constructors and destructor
+        public Game()
+        {
+
+        }
+        public Game(int ht, int wd)
+        {
+            height = ht;
+            width = wd;
+        }
+        ~Game()
+        {
+            
+        }
+
+        // Initialize the game, loading textures and resources as well
+        public void Initialize()
+        {
+            // Load shaders
+            ResourceManager.LoadShader("C:/Users/Cor/Documents/Visual Studio 2015/Projects/TestGL/TestGL/shaders/sprite.vs", "C:/Users/Cor/Documents/Visual Studio 2015/Projects/TestGL/TestGL/shaders/sprite.frag", null, "sprite");
+            // Configure shaders
+            Matrix4 projection = Matrix4.CreateOrthographic((float) width, (float) height, 0.0f, -1.0f);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+
+            ResourceManager.GetShader("sprite").Use().SetInteger("image", 0);
+            ResourceManager.GetShader("sprite").SetMatrix4("projection", projection);
+            // Load textures
+            ResourceManager.LoadTexture("C:/Users/Cor/Documents/Visual Studio 2015/Projects/TestGL/TestGL/textures/boy.png", true, "boy");
+            // Set render-specific controls
+            Shader myShader = ResourceManager.GetShader("sprite");
+            Renderer = new SpriteRenderer(myShader);
+        }
+
+        // Called every frame, takes time since last Update as argument
+        public void Update(double deltaTime)
+        {
+
+        }
+
+        // Code for rendering the current room goes here
+        public void Render()
+        {
+            Texture2D myTexture = ResourceManager.GetTexture("boy");
+
+            Renderer.DrawSprite(myTexture, new Vector2(200, 200), new Vector2(300, 400), 45.0f, new Vector3(0.0f, 1.0f, 0.0f));
+
+        }
+
+        public int LoadTexture(string file)
+        {
+            Bitmap bitmap = new Bitmap(file);
+
+            int tex;
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+            GL.GenTextures(1, out tex);
+            GL.BindTexture(TextureTarget.Texture2D, tex);
+
+            BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            bitmap.UnlockBits(data);
+
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            return tex;
+        }
+    }
+}
---
+++ TestGL/TestGL/SpriteRenderer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK;
+//using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TestGL
+{
+    class SpriteRenderer
+    {
+        public SpriteRenderer()
+        {
+            // Empty constructor
+        }
+
+        // Constructor (inits shaders/shapes)
+        public SpriteRenderer(Shader shader)
+        {
+
+        }
+        
+        // Destructor
+        ~SpriteRenderer()
+        {
+
+        }
+        
+        // Renders a defined quad textured with given sprite
+        public void DrawSprite(Texture2D texture, Vector2 position, Vector2 size = new Vector2(), float rotate = 0.0f, Vector3 color = new Vector3())
+        {
+            // Default values if none passed
+            if (size == null)
+            {
+                size = new Vector2(10f, 10f);
+            }
+            if (color == null)
+            {
+                color = new Vector3(1.0f, 1.0f, 1.0f);
+            }
+
+            // Prepare transformations
+            shader.Use();
+            Matrix4 model = Matrix4.Identity;
+            model = Matrix4.CreateTranslation(new Vector3(position[0], position[1], 0.0f));  // First translate (transformations are: scale happens first, then rotation and then finall translation happens; reversed order)
+
+            model *= Matrix4.CreateTranslation(new Vector3(0.5f * size[0], 0.5f * size[1], 0.0f)); // Move origin of rotation to center of quad
+            model *= Matrix4.CreateRotationZ(rotate); // Then rotate
+            model *= Matrix4.CreateTranslation(new Vector3(-0.5f * size[0], -0.5f * size[1], 0.0f)); // Move origin back
+
+            model = Matrix4.CreateScale(size[0], size[1], 1.0f); // Last scale
+            
+            shader.SetMatrix4("model", model);
+
+            // Render textured quad
+            shader.SetVector3f("spriteColor", color);
+            GL.ActiveTexture(TextureUnit.Texture0);
+            texture.Bind();
+
+            GL.BindVertexArray(quadVAO);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+            GL.BindVertexArray(0);
+            GL.Finish();
+            
+        }
+        
+        // Render state
+        private Shader shader = new Shader();
+
+        private uint quadVAO;
+
+        // Initializes and configures the quad's buffer and vertex attributes
+        private void initRenderData()
+        {
+            // Configure VAO/VBO
+            int VBO;
+            float[] vertices = { 
+                // Pos      // Tex
+                0.0f, 1.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 1.0f, 0.0f,
+                0.0f, 0.0f, 0.0f, 0.0f,
+
+                0.0f, 1.0f, 0.0f, 1.0f,
+                1.0f, 1.0f, 1.0f, 1.0f,
+                1.0f, 0.0f, 1.0f, 0.0f
+            };
+
+            GL.GenVertexArrays(1, out quadVAO);
+            GL.GenBuffers(1, out VBO);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr) (vertices.Length * sizeof(float)), vertices, BufferUsageHint.StaticDraw);
+
+            GL.BindVertexArray(quadVAO);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, 4 * sizeof(float), (IntPtr) null);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+        }
+    }
+}
